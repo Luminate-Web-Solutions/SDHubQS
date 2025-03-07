ba6432bc9d2c2e2b5132293e31d743c8bd482828
Iamfahad948 <fahadullah948@gmail.com>
2025-03-07 11:39:42 +0530

    started website and added comming soon page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate, state } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({
+        opacity: 0,
+        transform: 'translateY(20px)'
+      })),
+      transition(':enter', [
+        animate('0.6s ease-out', style({
+          opacity: 1,
+          transform: 'translateY(0)'
+        }))
+      ])
+    ])
+  ]
 })
-export class AppComponent {
-  title = 'SDHubQS';
+export class AppComponent implements OnInit{
+  title = 'SDHUB-QutubShahi';
+  progress = 0;
+  
+  ngOnInit() {
+    // Simulate progress
+    const interval = setInterval(() => {
+      this.progress += 1;
+      if (this.progress >= 100) {
+        clearInterval(interval);
+      }
+    }, 50);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { provideHttpClient  } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +17,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressBarModule,
+    BrowserAnimationsModule,
+
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(),
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
